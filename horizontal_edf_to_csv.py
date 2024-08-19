fce1fb0a09362f83d86b2329e2602d222d3d7532
Alexandros Apostolidis <ierosa2smix@gmail.com>
2024-08-19 19:49:06 +0200

    now we have a function that returns a df

--- horizontal_edf_to_csv.py
+++ horizontal_edf_to_csv.py
@@ -1,17 +1,23 @@
 import numpy as np
-import mne
+import mne, sys
 from mne.io import read_raw_edf
 import pandas as pd  # Import pandas for CSV handling
 
-# Load EDF file
-edf_file = '9_10_2.edf'
-raw = read_raw_edf(edf_file, preload=True)
+def edf_to_df(edf_path:str)-> pd.DataFrame:
+    raw = read_raw_edf(edf_path, preload=True)
+    eog_data, times = raw.copy().pick_channels(['EEGFp2CPz']).get_data(return_times=True)
+    return pd.DataFrame(data={'Time': times, 'C4': eog_data[0, :]})
 
-# Pick the horizontal EOG channel
-eog_data, times = raw.copy().pick_channels(['EEGFp2CPz']).get_data(return_times=True)
 
-# Save EOG data to CSV
-eog_df = pd.DataFrame(data={'Time': times, 'C4': eog_data[0, :]})
-eog_df.to_csv('y9_10_2one.csv', index=False)
 
-print("Horizontal EOG data saved to 'Fp2data.csv'")
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) <3:
+        edf_file = '9_10_2.edf'
+    else:
+        edf_file = args[-1]
+    df = edf_to_df(edf_path=edf_file)
+    df.to_csv('y9_10_2one.csv', index=False)
+    print("Horizontal EOG data saved to 'Fp2data.csv'")
+
+
